9edb30378c5196c71ad3b20ec60e4b9c7d2b2811
Christophe Rimann <cjr2185@columbia.edu>
2019-03-21 12:06:35 -0400

    contagion ec

--- HW2/contagion.py
+++ HW2/contagion.py
@@ -1,6 +1,6 @@
 from helper_functions import *
 import networkx as nx
-from random import randint
+from random import randint, choice
 # include any code you need for your assignment in this file or in auxiliary
 # files that can be imported here.
 
@@ -74,7 +74,7 @@ def q_9_b():
 	tot_infected_across_trials = 0
 	for i in range(100):
 		fb = create_fb_graph()
-		early = [randint(0, nx.number_of_nodes(fb)) for j in range(10)]
+		early = [randint(0, nx.number_of_nodes(fb)-1) for j in range(10)]
 		tot_infected_across_trials += num_infected(contagion_brd(fb, early, 0.1))
 	print(tot_infected_across_trials/float(100))
 
@@ -89,7 +89,41 @@ def q_9_c():
 			print("{},{},{}".format(q, k, tot_infected_across_trials/float(10)))
 
 
+def minimal_cascade(G, q, n_samples=100):
+	S_set = []
+	all_nodes = [i for i in range(nx.number_of_nodes(G))]
+	while True:
+		print("iter")
+		max_infected = 0
+		best_node = None
+		test_nodes = []
+		samples = list(set(all_nodes)^set(S_set))
+		while len(test_nodes) < n_samples:
+			selected_node = choice(samples)
+			test_nodes.append(selected_node)
+			samples.remove(selected_node)
+		for node in test_nodes:
+			temp_graph = G.copy()
+			test_S_set = S_set[:]
+			test_S_set.extend([node])
+			test_contagion = contagion_brd(temp_graph, test_S_set, q)
+			test_num_infected = num_infected(test_contagion)
+			if test_num_infected == len(G.nodes):
+				print(test_S_set)
+				return test_S_set
+			if test_num_infected > max_infected:
+				max_infected = test_num_infected
+				best_node = node
+			# print("{} infected {}".format(best_node, max_infected))
+		S_set.extend([best_node])
+
+def bonus():
+	fb = create_fb_graph()
+	for q in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]:
+		r = minimal_cascade(fb, q, 100)
+		print("{}:{}".format(q, len(r)))
+
 # q_9_a()
 # q_9_b()
-q_9_c()
-
+# q_9_c()
+bonus()
