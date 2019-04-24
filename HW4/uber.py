112583d9dad95d5c5ccb019873a47da0954e2e7a
Ryan Pencak <ryanpencak@Ryans-MBP.fios-router.home>
2019-04-23 22:34:12 -0400

    HW4 INIT

---
+++ HW4/uber.py
@@ -0,0 +1,15 @@
+# Some helper functions for Part 5
+
+# compute the manhattan distance between two points a and b (represented as pairs)
+def dist(a,b):
+    return -1
+
+# Give a representation for riders/ drivers somewhere which can be included in your graph used in stable_outcome
+
+# Given a (bipartite) graph G with edge values specified by v, 
+# output a stable outcome (M,a) consisting of a matching and allocations
+def stable_outcome(G,v)
+    M = None
+    a = None
+    return (M,a)
+
