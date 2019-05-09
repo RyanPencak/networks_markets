8fe550d89dbf105e3eb5b4977755dd0ad9b5696a
Ryan Pencak <ryanpencak@dhcp-nyc-1325.redrover.cornell.edu>
2019-05-09 16:40:39 -0400

    PageRank

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -4,9 +4,10 @@ authors = ['rvp32','cjr268','ds2346','ccs254']
 # Which version of python are you using? python 2 or python 3?
 python_version = "3"
 
-# Important: You are NOT allowed to modify the method signatures (i.e. the arguments and return types each function takes).
-
+import operator
 import networkx as nx
+
+# Important: You are NOT allowed to modify the method signatures (i.e. the arguments and return types each function takes).
 
 # Implement the methods in this class as appropriate. Feel free to add other methods
 # and attributes as needed.
@@ -14,7 +15,7 @@ import networkx as nx
 class DirectedGraph:
 
     def __init__(self,number_of_nodes):
-        self.G = nx.Graph()
+        self.G = nx.DiGraph()
 
         for n in range(number_of_nodes):
             self.G.add_node(n)
@@ -25,14 +26,12 @@ class DirectedGraph:
     def edges_from(self, origin_node):
         ''' This method shold return a list of all the nodes u such that the edge (origin_node,u) is
         part of the graph.'''
-        return list(self.G.adj(origin_node))
+        return list(self.G.adj[origin_node])
 
     def check_edge(self, origin_node, destination_node):
         ''' This method should return true is there is an edge between origin_node and destination_node
         and destination_node, and false otherwise'''
-        if (destination_node in list(self.G.adj(origin_node))):
-            return True
-        return False
+        return self.G.has_edge(origin_node,destination_node)
 
     def number_of_nodes(self):
         ''' This method should return the number of nodes in the graph'''
@@ -42,22 +41,75 @@ def scaled_page_rank(graph, num_iter, eps = 1/7.0):
     ''' This method, given a directed graph, should run the epsilon-scaled page-rank
     algorithm for num-iter iterations and return a mapping (dictionary) between a node and its weight.
     In the case of 0 iterations, all nodes should have weight 1/number_of_nodes'''
-    pass
+
+    scores = {}
+    n = graph.number_of_nodes()
+
+    # Set Score_0 to 1/n
+    for v in range(n):
+        scores[v] = [None] * (num_iter+1)
+        scores[v][0] = (1/n)
+
+    # Set Score_i+1
+    for i in range(num_iter):
+        for v in range(n):
+            summation = 0
+            for v_prime in graph.edges_from(v):
+                summation += (scores[v_prime][i]/len(graph.edges_from(v_prime)))
+
+            scores[v][i+1] = ((eps/n) + ((1 - eps) * summation))
+
+    for node in scores.keys():
+        scores[node] = scores[node][-1]
+
+    # for i in range(num_iter):
+    #     for v in range(n):
+    #         summation = 0
+    #         for v_prime in graph.edges_from(v):
+    #             summation += (scores[v_prime][i-1]/len(graph.edges_from(v_prime)))
+    #
+    #         scores[v] = ((eps/n) + ((1 - eps) * summation))
+
+    return scores
 
 def graph_15_1_left():
     ''' This method, should construct and return a DirectedGraph encoding the left example in fig 15.1
     Use the following indexes: A:0, B:1, C:2, Z:3 '''
-    pass
+    graph = DirectedGraph(4)
+    graph.add_edge(0,1)
+    graph.add_edge(1,2)
+    graph.add_edge(2,0)
+    graph.add_edge(0,3)
+    graph.add_edge(3,3)
+
+    return graph
 
 def graph_15_1_right():
     ''' This method, should construct and return a DirectedGraph encoding the right example in fig 15.1
     Use the following indexes: A:0, B:1, C:2, Z1:3, Z2:4'''
-    pass
+    graph = DirectedGraph(5)
+    graph.add_edge(0,1)
+    graph.add_edge(1,2)
+    graph.add_edge(2,0)
+    graph.add_edge(0,3)
+    graph.add_edge(0,4)
+    graph.add_edge(3,4)
+    graph.add_edge(4,3)
+
+    return graph
 
 def graph_15_2():
     ''' This method, should construct and return a DirectedGraph encoding example 15.2
         Use the following indexes: A:0, B:1, C:2, A':3, B':4, C':5'''
-    pass
+    graph = DirectedGraph(6)
+    graph.add_edge(0,1)
+    graph.add_edge(1,2)
+    graph.add_edge(2,0)
+    graph.add_edge(3,4)
+    graph.add_edge(4,5)
+    graph.add_edge(5,3)
+
+    return graph
 
 def extra_graph_1():
     ''' This method, should construct and return a DirectedGraph of your choice with at least 10 nodes'''
@@ -79,12 +131,57 @@ extra_graph_2_weights = {1 : 0, 2: 0, 3 : 0, 4: 0, 5 : 0, 6: 0, 7 : 0, 8: 0, 9 :
 def facebook_graph(filename = "facebook_combined.txt"):
     ''' This method should return a DIRECTED version of the facebook graph as an instance of the DirectedGraph class.
     In particular, if u and v are friends, there should be an edge between u and v and an edge between v and u.'''
-    pass
+
+    fb_G = DirectedGraph(4039)
+
+    # create graph from Facebook data
+    with open(filename) as in_file:
+        node_list = in_file.readlines()
+        node_list = [x.strip().split() for x in node_list]
+
+        for node_pair in node_list:
+            node_pair[0] = int(node_pair[0])
+            node_pair[1] = int(node_pair[1])
+
+    # Create edges for each pair
+    for node_pair in node_list:
+        fb_G.add_edge(node_pair[0],node_pair[1])
+        fb_G.add_edge(node_pair[1],node_pair[0])
+
+    return fb_G
 
 
 # The code necessary for your measurements for question 8b should go in this function.
 # Please COMMENT THE LAST LINE OUT WHEN YOU SUBMIT (as it will be graded by hand and we do not want it to interfere
 # with the automatic grader).
 def question8b():
-    pass
-#question8b()
+    fb_G = facebook_graph()
+    fb_weights = scaled_page_rank(fb_G, 15) # run Pagerank with 15 rounds
+
+    max_weight_node = max(fb_weights.items(), key=operator.itemgetter(1))[0]
+    min_weight_node = min(fb_weights.items(), key=operator.itemgetter(1))[0]
+
+    # Print sorted dictionary of weights from Pagerank algorithm
+    sorted_weights = sorted(fb_weights.items(), key=lambda x: x[1])
+    print("Node : Weight : Out-degree")
+    for pair in sorted_weights:
+        print(str(pair[0]) + " : " + str(pair[1]) + " : " + str(len(fb_G.edges_from(pair[0]))))
+
+    # Print information on node with maximum score
+    print("\n\nNode with max score:" + str(max_weight_node))
+    print("Max score: " + str(fb_weights[max_weight_node]))
+    # print("Edges from max score node: " + str(fb_G.edges_from(max_weight_node)))
+    print("Out-degree of max score node: " + str(len(fb_G.edges_from(max_weight_node))))
+
+    # Print information on node with maximum score
+    print("Node with min score :" + str(min_weight_node))
+    print("Min score of node: " + str(fb_weights[min_weight_node]))
+    # print("Edges from min score node: " + str(fb_G.edges_from(min_weight_node)))
+    print("Out-degree of min score node: " + str(len(fb_G.edges_from(min_weight_node))))
+
+    # Print sum of weights
+    print("\nSum of weights: " + str(sum(fb_weights.values())))
+
+    return (fb_weights)
+
+# question8b()
--- HW4/tester.py
+++ HW4/tester.py
@@ -1,22 +1,113 @@
 import hw4
+import networkx as nx
 
 # DO NOT SUBMIT THIS FILE
 # It is just an example of a few tests that we will run on your code that you can use as a starting point
 # to make sure the code is correct.
 # You should put the two python files in the same folder and run this one
 
+# Basic Testing
+print("======================")
+print("BASIC TESTING")
+print("======================")
 testGraph = hw4.DirectedGraph(5)
-
 assert testGraph.number_of_nodes() == 5
-
 assert testGraph.check_edge(0,1) == False
-
 testGraph.add_edge(0,1)
-
 assert testGraph.check_edge(0,1) == True
+scores = hw4.scaled_page_rank(testGraph,0)
+assert scores[2] == 1/5.0
+print("done.")
 
-weights = hw4.scaled_page_rank(testGraph,0)
+# Graph 15.1 Left Testing
+print("======================")
+print("GRAPH 15.1 Left TESTING")
+print("======================")
+example_15_1_left = hw4.graph_15_1_left()
+assert example_15_1_left.number_of_nodes() == 4
+assert example_15_1_left.edges_from(0) == [1,3]
+assert example_15_1_left.edges_from(1) == [2]
+assert example_15_1_left.edges_from(2) == [0]
+assert example_15_1_left.edges_from(3) == [3]
+assert example_15_1_left.check_edge(0,1)
+assert example_15_1_left.check_edge(1,2)
+assert example_15_1_left.check_edge(2,0)
+assert example_15_1_left.check_edge(0,3)
+assert example_15_1_left.check_edge(3,3)
 
-assert weights[2] == 1/5.0
+scores_0 = hw4.scaled_page_rank(example_15_1_left,0)
+print("Example 15.1 Left - Scores - 0 Rounds: " + str(scores_0))
+print("Sum of scores: " + str(sum(scores_0.values())))
 
-assert hw4.graph_15_1_left().number_of_nodes() == 4
+scores_10 = hw4.scaled_page_rank(example_15_1_left,10)
+print("Example 15.1 Left - Scores - 10 Rounds: " + str(scores_10))
+print("Sum of scores: " + str(sum(scores_10.values())))
+
+scores_20 = hw4.scaled_page_rank(example_15_1_left,20)
+print("Example 15.1 Left - Scores - 20 Rounds: " + str(scores_20))
+print("Sum of scores: " + str(sum(scores_20.values())))
+
+nx_example_15_1_left = nx.DiGraph()
+nx_example_15_1_left.add_edge(0,1)
+nx_example_15_1_left.add_edge(1,2)
+nx_example_15_1_left.add_edge(2,0)
+nx_example_15_1_left.add_edge(0,3)
+nx_example_15_1_left.add_edge(3,3)
+correct_scores = nx.pagerank(nx_example_15_1_left)
+print("Correct scores: " + str(correct_scores))
+print("Sum of correct scores: " + str(sum(correct_scores.values())))
+
+
+# Graph 15.1 Right Testing
+print("======================")
+print("GRAPH 15.1 RIGHT TESTING")
+print("======================")
+example_15_1_right = hw4.graph_15_1_right()
+assert example_15_1_right.number_of_nodes() == 5
+assert example_15_1_right.edges_from(0) == [1,3,4]
+assert example_15_1_right.check_edge(0,1)
+assert example_15_1_right.check_edge(1,2)
+assert example_15_1_right.check_edge(2,0)
+assert example_15_1_right.check_edge(0,3)
+assert example_15_1_right.check_edge(0,4)
+assert example_15_1_right.check_edge(3,4)
+assert example_15_1_right.check_edge(4,3)
+
+scores_0 = hw4.scaled_page_rank(example_15_1_right,0)
+print("Example 15.1 Right - Scores - 0 Rounds: " + str(scores_0))
+print("Sum of scores: " + str(sum(scores_0.values())))
+
+scores_10 = hw4.scaled_page_rank(example_15_1_right,10)
+print("Example 15.1 Right - Scores - 10 Rounds: " + str(scores_10))
+print("Sum of scores: " + str(sum(scores_10.values())))
+
+scores_20 = hw4.scaled_page_rank(example_15_1_right,20)
+print("Example 15.1 Right - Scores - 20 Rounds: " + str(scores_20))
+print("Sum of scores: " + str(sum(scores_20.values())))
+
+
+# Graph 15.2 Testing
+print("======================")
+print("GRAPH 15.2 TESTING")
+print("======================")
+example_15_2 = hw4.graph_15_2()
+assert example_15_2.number_of_nodes() == 6
+assert example_15_2.edges_from(0) == [1]
+assert example_15_2.check_edge(0,1)
+assert example_15_2.check_edge(1,2)
+assert example_15_2.check_edge(2,0)
+assert example_15_2.check_edge(3,4)
+assert example_15_2.check_edge(4,5)
+assert example_15_2.check_edge(5,3)
+
+scores_0 = hw4.scaled_page_rank(example_15_2,0)
+print("Example 15.2 - Scores - 0 Rounds: " + str(scores_0))
+print("Sum of scores: " + str(sum(scores_0.values())))
+
+scores_10 = hw4.scaled_page_rank(example_15_2,10)
+print("Example 15.2 - Scores - 10 Rounds: " + str(scores_10))
+print("Sum of scores: " + str(sum(scores_10.values())))
+
+scores_20 = hw4.scaled_page_rank(example_15_2,20)
+print("Example 15.2 - Scores - 20 Rounds: " + str(scores_20))
+print("Sum of scores: " + str(sum(scores_20.values())))
